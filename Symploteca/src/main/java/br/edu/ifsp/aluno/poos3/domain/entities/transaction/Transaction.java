11da6a953dab3ea1442678d57aa628c29eeecf17
Lucas Silva de Almeida <lucas.lucas.sa@hotmail.com>
2023-02-08 11:38:44 -0300

    Entidade Transaction
    Implementação da classe Transaction.

---
+++ Symploteca/src/main/java/br/edu/ifsp/aluno/poos3/domain/entities/transaction/Transaction.java
@@ -0,0 +1,69 @@
+package br.edu.ifsp.aluno.poos3.domain.entities.transaction;
+
+import br.edu.ifsp.aluno.poos3.domain.entities.book.Book;
+import br.edu.ifsp.aluno.poos3.domain.entities.user.User;
+
+import java.time.LocalDate;
+
+public class Transaction {
+    private Integer id;
+    private final LocalDate issueDate;
+    private final LocalDate dueDate;
+    private LocalDate returnDate;
+    private final Book book;
+    private final User user;
+
+    public Transaction(Book book, User user) {
+        this.book = book;
+        this.user = user;
+        this.issueDate = LocalDate.now();
+        this.dueDate = issueDate.plusDays(user.getCheckOutTimeLimitInDays());
+    }
+
+    public Transaction(Integer id, LocalDate issueDate,
+                       LocalDate dueDate, LocalDate returnDate,
+                       Book book, User user) {
+        this.id = id;
+        this.issueDate = issueDate;
+        this.dueDate = dueDate;
+        this.returnDate = returnDate;
+        this.book = book;
+        this.user = user;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDate getIssueDate() {
+        return issueDate;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "id=" + id +
+                ", issueDate=" + issueDate +
+                ", dueDate=" + dueDate +
+                ", returnDate=" + returnDate +
+                ", book=" + book +
+                ", user=" + user +
+                '}';
+    }
+}
